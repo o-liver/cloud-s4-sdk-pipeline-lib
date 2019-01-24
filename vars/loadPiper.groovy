b7b525eb8c8f830b6d9a7bc8f866a07a79809de9
Florian Wilhelm <florian.wilhelm02@sap.com>
2019-01-24 17:34:33 +0100

    Rename lib

--- vars/loadPiper.groovy
+++ vars/loadPiper.groovy
@@ -1,4 +1,4 @@
 def call(Map parameters = [:]) {
     // If you change the version please also update build.gradle and the corresponding jar file
-    library 'piper-library-os@32e20a72936616b4aebeb5aed5513fbe6102e99d'
+    library 'piper-lib-os@32e20a72936616b4aebeb5aed5513fbe6102e99d'
 }
